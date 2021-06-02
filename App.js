00916f2515c05fccf9587ec5d7db32659b5c4984
Playbird12 <83607406+Playbird12@users.noreply.github.com>
2021-06-01 19:49:38 -0500

    added buttons

--- App.js
+++ App.js
@@ -1,19 +1,19 @@
 import React, { useState } from 'react';
-import { Text, TextInput, TextPropTypes, View, StyleSheet } from 'react-native';
+import { Text, TextInput, View, StyleSheet, Button } from 'react-native';
 
 const Textbox = (props) => {
   const [text, setText] = useState('')
 
   return (
-    <View> 
-      <TextInput 
-        style = {styles.textbox}
-        placeholder = {props.placeholder}
-        onChangeText = {text => setText(text)}
-        defaultValue = {text}
+    <View>
+      <TextInput
+        style={styles.textbox}
+        placeholder={props.placeholder}
+        onChangeText={text => setText(text)}
+        defaultValue={text}
       />
 
-      <Text style = {styles.blackRegular}>
+      <Text style={styles.blackRegular}>
         {text.split(' ').map((word) => word && '🍕').join(' ')}
       </Text>
 
@@ -22,15 +22,34 @@ const Textbox = (props) => {
 
 }
 
+const Cat = (props) => {
+
+  return (
+    <View>
+      <Text style = {styles.blackRegular, {padding: 10} }>
+        I am person who is cool
+      </Text>
+      <Button
+        color="#841584"
+        onPress={() => {
+          alert('fuck off');
+        }}
+        title = {"fun time"}
+      />
+    </View>
+  );
+}
+
+
 const styles = StyleSheet.create({
   textbox: {
     marginTop: 50,
-    padding: 100,
+    padding: 5,
   },
   blackRegular: {
     color: 'red',
     fontWeight: 'normal',
-    fontSize: 42,
+    fontSize: 10,
     padding: 10
   }
 })
@@ -38,7 +57,8 @@ const styles = StyleSheet.create({
 const main = () => {
   return (
     <>
-       <Textbox placeholder = 'type text here'/>
+      <Textbox placeholder='type text here' />
+      <Cat name="Munkustrap" />
     </>
   )
 }
